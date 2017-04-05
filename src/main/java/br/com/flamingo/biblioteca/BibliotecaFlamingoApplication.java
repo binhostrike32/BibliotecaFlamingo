53b7be342b5096cf3e1423de00dbe19c5cc88ae1
FonsekaLucas <fonsekalucas@gmail.com>
2017-04-05 17:05:58 -0300

    Add files via upload

---
+++ src/main/java/br/com/flamingo/biblioteca/BibliotecaFlamingoApplication.java
@@ -0,0 +1,12 @@
+package br.com.flamingo.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaFlamingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaFlamingoApplication.class, args);
+	}
+}
